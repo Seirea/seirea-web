b60a61bfc51737425bd8d31d898c3aded01b27b1
stanleymw <107821509+stanleymw@users.noreply.github.com>
2024-11-29 20:01:57 -0800

    fix string type

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,28 @@
 <script>
-    import { generateKey } from "../lib/auth/keygen";
-    console.log(generateKey())
+  import { generateKey } from "../lib/auth/keygen";
+  console.log(generateKey());
 </script>
 
-
 <main class="max-w-xl mx-auto p-4">
-    <h1 class="transition-all text-4xl hover:text-red-500 hover:font-bold"><a href="/about">Seirea</a></h1>
-    <form class="flex flex-col gap-2">
-        <input type="email" class="form-input rounded-md shadow-md" placeholder="Email"/>
-        <input type="password" class="form-input rounded-md shadow-md" placeholder="Password"/>
+  <h1 class="transition-all text-4xl hover:text-red-500 hover:font-bold">
+    <a href="/about">Seirea</a>
+  </h1>
+  <form class="flex flex-col gap-2">
+    <input
+      type="email"
+      class="form-input transition-all rounded-md shadow-md hover:border-blue-400"
+      placeholder="Email"
+    />
+    <input
+      type="password"
+      class="form-input transition-all rounded-md shadow-md hover:border-blue-400"
+      placeholder="Password"
+    />
 
-        <input type="submit" class="form-input rounded-md shadow-md" name="Submit"/>
-    </form>
+    <input
+      type="submit"
+      class="form-input transition-all rounded-md shadow-md hover:border-blue-400 hover:bg-gradient-to-tr hover:from-blue-200 hover:to-red-200 "
+      name="Log in"
+    />
+  </form>
 </main>
-
