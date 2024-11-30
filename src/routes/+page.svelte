9e367229144df6c7c7de14ba76a4d791318358a4
stanleymw <107821509+stanleymw@users.noreply.github.com>
2024-11-29 20:16:42 -0800

    add login form

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,30 @@
-<script>
+<script lang="ts">
   import { generateKey } from "../lib/auth/keygen";
   console.log(generateKey());
+
+  function handleLoginSubmit(e: SubmitEvent) {
+    const formData = new FormData(e.target as HTMLFormElement);
+    console.log(formData);
+  }
 </script>
 
 <main class="max-w-xl mx-auto p-4">
   <h1 class="transition-all text-4xl hover:text-red-500 hover:font-bold">
     <a href="/about">Seirea</a>
   </h1>
-  <form class="flex flex-col gap-2">
+  <form
+    class="flex flex-col gap-2"
+    on:submit|preventDefault={handleLoginSubmit}
+  >
     <input
       type="email"
+      name="email"
       class="form-input transition-all rounded-md shadow-md hover:border-blue-400"
       placeholder="Email"
     />
     <input
       type="password"
+      name="password"
       class="form-input transition-all rounded-md shadow-md hover:border-blue-400"
       placeholder="Password"
     />
