f3b738db480855dc05b000e40bbd8646f911b40d
stanleymw <107821509+stanleymw@users.noreply.github.com>
2024-11-29 22:48:06 -0800

    FIX PROXY IT WORKS LETS GOOOO CORS IS EVIL

--- src/routes/proxy/+server.ts
+++ src/routes/proxy/+server.ts
@@ -2,9 +2,9 @@ import type { RequestHandler } from "./$types";
 
 export const POST: RequestHandler = async ({ request }) => {
 	let { url, ...body } = await request.json();
-	console.log(body);
 	return await fetch(url, {
 		method: "POST",
-		body: body,
+		headers: [["Content-Type", "application/json"]],
+		body: JSON.stringify(body),
 	});
 };
