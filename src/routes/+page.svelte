0baeabc466ce4b38dc445f0fcc3bc8bba08f7504
stanleymw <107821509+stanleymw@users.noreply.github.com>
2024-11-29 19:46:24 -0800

    update ui and add about page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,12 +5,12 @@
 
 
 <main class="max-w-xl mx-auto p-4">
-    <h1 class="text-4xl font-display">Seirea</h1>
+    <h1 class="transition-all text-4xl hover:text-red-500 hover:font-bold"><a href="/about">Seirea</a></h1>
     <form class="flex flex-col gap-2">
         <input type="email" class="form-input rounded-md shadow-md" placeholder="Email"/>
         <input type="password" class="form-input rounded-md shadow-md" placeholder="Password"/>
 
-        <input type="submit" class="form-input rounded-md shadow-md" name="Submit" />
+        <input type="submit" class="form-input rounded-md shadow-md" name="Submit"/>
     </form>
 </main>
 
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+    import Button from "$lib/components/Button.svelte";
+
+</script>
+
+<main class="prose p-4">
+    <h1>
+        <Button href="/">
+            Seirea
+        </Button>
+    </h1>
+    <h2>What is Seirea?</h2>
+    <p>Seirea is an open source grade viewer. You can view the grades of all your classes and assignments.</p>
+
+    <p>
+        Seirea is created by
+        <span class="font-bold">Stanley Wang</span> 
+        and 
+        <span class="font-bold">Yavor Kolev</span>.
+    </p>
+</main>
