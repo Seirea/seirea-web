023fdaf687eff3ec5866fc3703565668282db443
stanleymw <107821509+stanleymw@users.noreply.github.com>
2024-11-29 18:05:29 -0800

    add pages

---
+++ src/routes/home/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+    let {children} = $props();
+</script>
+
+<nav class="flex flex-row justify-between p-2">
+    <h1>Seirea</h1>
+
+    <div class="flex flex-row justify-between gap-4">
+        <a href="/home/classes">Classes</a>
+        <a href="/home/grades">Grades</a>
+        <a href="/home/settings">Settings</a>
+    </div>
+</nav>
+
+{@render children()}
