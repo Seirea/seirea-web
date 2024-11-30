53a27b883b5d1fcee6841b0f25d11a453db1c722
stanleymw <107821509+stanleymw@users.noreply.github.com>
2024-11-29 17:58:16 -0800

    add form back

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<main class="max-w-xl mx-auto p-4">
+    <h1 class="text-4xl font-display">Seirea</h1>
+    <form class="flex flex-col gap-2">
+        <input type="email" class="form-input rounded-md shadow-md" placeholder="Email"/>
+        <input type="password" class="form-input rounded-md shadow-md" placeholder="Password"/>
+
+        <input type="submit" class="form-input rounded-md shadow-md" name="Submit" />
+    </form>
+</main>
+
