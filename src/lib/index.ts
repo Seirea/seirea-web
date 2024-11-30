1c8792c648c07e85a2957e33498ed35c963d5319
yavko <yavornkolev@gmail.com>
2024-11-29 20:14:27 -0800

    export items w/ names

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,3 @@
 // place files you want to import through the `$lib` alias in this folder.
-export * from "./api";
-export * from "./auth/keygen";
+export * as api from "./api";
+export * as keygen from "./auth/keygen";
