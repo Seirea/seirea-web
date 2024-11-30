6b7064b5d7877b0cf309cea450f92a98aeef3b42
yavko <yavornkolev@gmail.com>
2024-11-29 23:11:03 -0800

    add logout

--- src/routes/home/settings/+page.svelte
+++ src/routes/home/settings/+page.svelte
@@ -1,1 +1,12 @@
+<script lang="ts">
+	import { goto } from "$app/navigation";
+
+	function logout() {
+		localStorage.removeItem("username");
+		localStorage.removeItem("password");
+		goto("/");
+	}
+</script>
+
 <h1>Settings Page</h1>
+<button on:click={logout}>Log out</button>
