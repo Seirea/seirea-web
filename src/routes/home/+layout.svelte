4e659fc7dfec2dabe40a3476ec9ee24bf41d037e
paranthropusgoober <180814340+paranthropusgoober@users.noreply.github.com>
2024-12-12 11:53:46 -0800

    move auth logic to login page and store login data (token and student data) in localstorage

--- src/routes/home/+layout.svelte
+++ src/routes/home/+layout.svelte
@@ -1,34 +1,6 @@
 <script lang="ts">
   import Button from "$lib/components/Button.svelte";
-  import { goto } from "$app/navigation";
-  import { AeriesApi } from "$lib/api";
-  import { onMount } from "svelte";
-
   let { children } = $props();
-
-  function failedLogin(msg: string) {
-    alert(`${msg}, returning to login page`);
-    goto("/");
-  }
-
-  onMount(async () => {
-    const username = localStorage.getItem("username");
-    const password = localStorage.getItem("password");
-    const apiUrl =
-      localStorage.getItem("api-url") ??
-      "https://aeries16.fjuhsd.org/parent/mobileapi/v1";
-    if (username == null || password == null) {
-      failedLogin("username or password not set");
-    } else {
-      const api = new AeriesApi(new URL(apiUrl));
-      const authed = await api.authenticate(username, password);
-      if (!authed) {
-        localStorage.clear();
-        alert("Unable to authenticate! Check your Email and Password.");
-        goto("/");
-      }
-    }
-  });
 </script>
 
 <nav class="flex flex-row justify-between p-2">
