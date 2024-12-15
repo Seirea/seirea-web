2939fa3c19521631664cd1e33c5e5b28c394cc5a
stanleymw <107821509+stanleymw@users.noreply.github.com>
2024-12-14 22:16:12 -0800

    fix

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -2,6 +2,6 @@
 </script>
 
 <div class="flex flex-col gap-4 p-4">
-  <h1 class="text-4xl">Home</h1>
-  <div class="flex flex-col"></div>
+	<h1 class="text-4xl">Home</h1>
+	<div class="flex flex-col"></div>
 </div>
