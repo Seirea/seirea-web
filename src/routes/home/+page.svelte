6cb80e82d7e6e49d25e533f546c4d98854d085df
stanleymw <107821509+stanleymw@users.noreply.github.com>
2024-12-14 22:09:07 -0800

    Revert "move auth logic to login page and store login data (token and student data) in localstorage"
    This reverts commit 4e659fc7dfec2dabe40a3476ec9ee24bf41d037e.

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,51 +1,7 @@
 <script lang="ts">
-	import { onMount } from "svelte";
-	import { goto } from "$app/navigation";
-	import { AeriesApi } from "$lib/api";
-	import type { Assignment } from "$lib/api-types";
-
-	let assignments: Assignment[] = [];
-
-	onMount(async () => {
-		const apiUrl = localStorage.getItem("api-url");
-		const authData = JSON.parse(localStorage.getItem("authData") ?? "{}");
-		if (apiUrl == null || authData == null) {
-			alert("You are not logged in! Redirecting to login page...");
-			await goto("/");
-		} else {
-			console.log(authData);
-			const api = new AeriesApi(new URL(apiUrl), authData.token, authData.student);
-			const homedata = await api.getHomePage();
-			assignments = homedata.RecentChanges;
-			console.warn(homedata);
-
-		}
-	});
-
 </script>
 
 <div class="flex flex-col gap-4 p-4">
-	<h1 class="text-4xl">Home</h1>
-	<div class="flex flex-col">
-		<table>
-			<thead>
-			<tr>
-				<th>Assignment Name</th>
-				<th>Category</th>
-				<th>Score</th>
-				<th>Max Score</th>
-			</tr>
-			</thead>
-			<tbody>
-			{#each assignments as assignment}
-				<tr style:background-color={assignment.GradingCompleted ? "inherit" : "gray"}>
-					<td>{assignment.AssignmentName}</td>
-					<td>{assignment.CategoryDescription}</td>
-					<td>{assignment.Score}</td>
-					<td>{assignment.MaxScore}</td>
-				</tr>
-			{/each}
-			</tbody>
-		</table>
-	</div>
+  <h1 class="text-4xl">Home</h1>
+  <div class="flex flex-col"></div>
 </div>
