dd3e770b56d59bd153369c1046670814d83a9b51
stanleymw <107821509+stanleymw@users.noreply.github.com>
2024-12-14 23:39:30 -0800

    try some store stuff

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,12 +1,17 @@
 <script lang="ts">
 	import type { AeriesApi } from "$lib/api";
 	import { getContext } from "svelte";
+	import { type Writable } from "svelte/store";
 
-	const api: AeriesApi = getContext("api");
-	console.log(api.getHomePage());
+	const api: Writable<AeriesApi> = getContext("api");
+	api.subscribe((val) => {
+		console.log("UPDATED!", val);
+	});
 </script>
 
 <div class="flex flex-col gap-4 p-4">
 	<h1 class="text-4xl">Home</h1>
-	<div class="flex flex-col"></div>
+	<div class="flex flex-col">
+		<p1>{$api.authedStudent}</p1>
+	</div>
 </div>
