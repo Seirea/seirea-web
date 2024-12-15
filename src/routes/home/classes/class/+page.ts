e799b93d4cc9eb24c6a46280749b428835aef4d6
stanleymw <107821509+stanleymw@users.noreply.github.com>
2024-12-14 22:08:48 -0800

    Revert move auth
    This reverts commit ec7e12581e8c9e443cdf32aa86c973e7ef11acc4.

--- src/routes/home/classes/class/+page.ts
+++
@@ -1,9 +0,0 @@
-import { error } from '@sveltejs/kit';
-import type { PageLoad } from './$types';
-import { authAndInitialize } from '$lib/api';
-
-export const load: PageLoad = async ({ url }) => {
-    let classId = url.searchParams.get('classId');
-
-    return { classId: classId ? parseInt(classId) : null };
-}
