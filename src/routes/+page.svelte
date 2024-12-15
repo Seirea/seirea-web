791f5bfb73bfd3a8d894726ecb6cab21bd9a4b36
stanleymw <107821509+stanleymw@users.noreply.github.com>
2024-12-14 22:16:12 -0800

    fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,52 +1,52 @@
 <script lang="ts">
-  import { goto } from "$app/navigation";
-  import { onMount } from "svelte";
+	import { goto } from "$app/navigation";
+	import { onMount } from "svelte";
 
-  onMount(() => {
-    if (
-      localStorage.getItem("username") != null &&
-      localStorage.getItem("password") != null
-    ) {
-      goto("/home");
-    }
-  });
+	onMount(() => {
+		if (
+			localStorage.getItem("username") != null &&
+			localStorage.getItem("password") != null
+		) {
+			goto("/home");
+		}
+	});
 
-  function handleLoginSubmit(e: SubmitEvent) {
-    const formData = new FormData(e.target as HTMLFormElement);
+	function handleLoginSubmit(e: SubmitEvent) {
+		const formData = new FormData(e.target as HTMLFormElement);
 
-    localStorage.setItem("username", formData.get("email") as string);
-    localStorage.setItem("password", formData.get("password") as string);
+		localStorage.setItem("username", formData.get("email") as string);
+		localStorage.setItem("password", formData.get("password") as string);
 
-    goto("/home");
-  }
+		goto("/home");
+	}
 </script>
 
 <main class="max-w-xl mx-auto p-4">
-  <h1 class="transition-all text-4xl hover:text-red-500 hover:font-bold">
-    <a href="/about">Seirea</a>
-  </h1>
+	<h1 class="transition-all text-4xl hover:text-red-500 hover:font-bold">
+		<a href="/about">Seirea</a>
+	</h1>
 
-  <form
-    class="flex flex-col gap-2"
-    on:submit|preventDefault={handleLoginSubmit}
-  >
-    <input
-      type="email"
-      name="email"
-      class="form-input transition-all rounded-md shadow-md hover:border-blue-400"
-      placeholder="Email"
-    />
-    <input
-      type="password"
-      name="password"
-      class="form-input transition-all rounded-md shadow-md hover:border-blue-400"
-      placeholder="Password"
-    />
+	<form
+		class="flex flex-col gap-2"
+		on:submit|preventDefault={handleLoginSubmit}
+	>
+		<input
+			type="email"
+			name="email"
+			class="form-input transition-all rounded-md shadow-md hover:border-blue-400"
+			placeholder="Email"
+		/>
+		<input
+			type="password"
+			name="password"
+			class="form-input transition-all rounded-md shadow-md hover:border-blue-400"
+			placeholder="Password"
+		/>
 
-    <input
-      type="submit"
-      class="form-input cursor-pointer transition-all rounded-md shadow-md hover:border-blue-400 hover:bg-blue-200"
-      name="Log in"
-    />
-  </form>
+		<input
+			type="submit"
+			class="form-input cursor-pointer transition-all rounded-md shadow-md hover:border-blue-400 hover:bg-blue-200"
+			name="Log in"
+		/>
+	</form>
 </main>
--- src/routes/home/+layout.svelte
+++ src/routes/home/+layout.svelte
@@ -1,43 +1,45 @@
 <script lang="ts">
-  import Button from "$lib/components/Button.svelte";
-  import { goto } from "$app/navigation";
-  import { AeriesApi } from "$lib/api";
-  import { onMount } from "svelte";
+	import Button from "$lib/components/Button.svelte";
+	import { goto } from "$app/navigation";
+	import { AeriesApi } from "$lib/api";
+	import { onMount } from "svelte";
 
-  let { children } = $props();
+	let { children } = $props();
 
-  function failedLogin(msg: string) {
-    alert(`${msg}, returning to login page`);
-    goto("/");
-  }
+	function failedLogin(msg: string) {
+		alert(`${msg}, returning to login page`);
+		goto("/");
+	}
 
-  onMount(async () => {
-    const username = localStorage.getItem("username");
-    const password = localStorage.getItem("password");
-    const apiUrl =
-      localStorage.getItem("api-url") ??
-      "https://aeries16.fjuhsd.org/parent/mobileapi/v1";
-    if (username == null || password == null) {
-      failedLogin("username or password not set");
-    } else {
-      const api = new AeriesApi(new URL(apiUrl));
-      const authed = await api.authenticate(username, password);
-      if (!authed) {
-        localStorage.clear();
-        alert("Unable to authenticate! Check your Email and Password.");
-        goto("/");
-      }
-    }
-  });
+	onMount(async () => {
+		const username = localStorage.getItem("username");
+		const password = localStorage.getItem("password");
+		const apiUrl =
+			localStorage.getItem("api-url") ??
+			"https://aeries16.fjuhsd.org/parent/mobileapi/v1";
+
+		if (username == null || password == null) {
+			failedLogin("username or password not set");
+		} else {
+			const api = new AeriesApi(new URL(apiUrl), null, null);
+
+			const authed = await api.authenticate(username, password);
+			if (!authed) {
+				localStorage.clear();
+				alert("Unable to authenticate! Check your Email and Password.");
+				goto("/");
+			}
+		}
+	});
 </script>
 
 <nav class="flex flex-row justify-between p-2">
-  <Button href="/home">Seirea</Button>
+	<Button href="/home">Seirea</Button>
 
-  <div class="flex flex-row justify-between gap-4">
-    <Button href="/home/classes">Classes</Button>
-    <Button href="/home/settings">Settings</Button>
-  </div>
+	<div class="flex flex-row justify-between gap-4">
+		<Button href="/home/classes">Classes</Button>
+		<Button href="/home/settings">Settings</Button>
+	</div>
 </nav>
 
 {@render children()}
