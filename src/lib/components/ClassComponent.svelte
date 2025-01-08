8c048c371d4f78b7dc51d8214c406fa4ccd8b83d
paranthropusgoober <180814340+paranthropusgoober@users.noreply.github.com>
2025-01-08 13:46:17 -0800

    move class info into component

---
+++ src/lib/components/ClassComponent.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import type { ClassSummary } from "$lib/api-types";
+	import Box from "$lib/components/Box.svelte";
+
+	interface Props {
+		classSummary: ClassSummary;
+	}
+
+	let p: Props = $props();
+
+	let lastUpdated;
+	if ((lastUpdated = p.classSummary.LastUpdated))
+		lastUpdated = lastUpdated.substring(6, lastUpdated.length - 2);
+	else lastUpdated = "Not Yet Updated";
+</script>
+
+<a href={"./classes/class?classId=" + p.classSummary.GradeBookNumber.toString()}>
+	<Box
+		title={p.classSummary.CourseTitle}
+		right={`${p.classSummary.CurrentMark} (${p.classSummary.Average || p.classSummary.Percent}%)`}
+		titlesubtext={p.classSummary.GradeBookName}
+		rightsubtext={lastUpdated}
+	/>
+</a>
